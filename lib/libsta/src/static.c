1cd645a9e94c5dfa06ea124dd2012f4e61fce9ff
tomasbrigido <tomas.brigido@mi.unc.edu.ar>
2024-03-21 23:46:32 -0300

    Testing

--- lib/libsta/src/static.c
+++ lib/libsta/src/static.c
@@ -4,6 +4,6 @@
 
 int static_lib_function(char* msg)
 {
-    printf("Hi! I'm a static lib that receives '%s', and return '%d'\n", msg, strlen(msg));
+    printf("Hi! I'm a static lib that receives '%s', and return '%ld'\n", msg, strlen(msg));
     return strlen(msg);
 }

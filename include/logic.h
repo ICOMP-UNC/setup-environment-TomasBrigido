2942d1f54ad337a69a78e53a8e2ca2d9efc8c663
tomasbrigido <tomas.brigido@mi.unc.edu.ar>
2024-03-24 16:47:00 -0300

    Forgot to save one file -.-

--- include/logic.h
+++ include/logic.h
@@ -1,11 +1,37 @@
 #pragma once
 
 /**
- * @brief Greet the user
+ * @brief Add two numbers
  *
+ * @param first_number First number
+ * @param second_number Second number
+ * @return firs_number + second_number
  */
-
 int add(int first_number, int second_number);
+
+/**
+ * @brief Subtract two numbers
+ *
+ * @param first_number First number
+ * @param second_number Second number
+ * @return firs_number - second_number
+ */
 int subtract(int first_number, int second_number);
+
+/**
+ * @brief Multiply two numbers
+ *
+ * @param first_number First number
+ * @param second_number Second number
+ * @return firs_number * second_number
+ */
 int multiply(int first_number, int second_number);
+
+/**
+ * @brief Divide two numbers
+ *
+ * @param first_number First number
+ * @param second_number Second number
+ * @return firs_number / second_number. if second_number != 0
+ */
 double divide(int first_number, int second_number);
